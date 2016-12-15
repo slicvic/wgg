c38909723843c9a5037378e440c8b75cba032043
Victor Lantigua <victor@Victors-MacBook-Pro.local>
2016-12-15 17:36:14 -0500

    Tidy up test data provider

--- tests/IpInfoIpGeolocatorTest.php
+++ tests/IpInfoIpGeolocatorTest.php
@@ -28,30 +28,21 @@ class IpInfoIpGeolocatorTest extends TestCase
             'Test 3 location not found' => ['', null],
             'Test 4 location not found' => ['1000', null],
             'Test 5 location not found' => ['600.700.800.900', null],
-            'Test 6 location found' => [
-                gethostbyname('google.com'),
-                [
+            'Test 6 location found'     => [gethostbyname('google.com'), [
                     'city' => 'Mountain View, California',
                     'lat' => '37.4192',
                     'lng' => '-122.0574'
-                ]
-            ],
-            'Test 7 location found' => [
-                gethostbyname('yahoo.com'),
-                [
+                ]],
+            'Test 7 location found'     => [gethostbyname('yahoo.com'), [
                     'city' => 'Sunnyvale, California',
                     'lat' => '37.4249',
                     'lng' => '-122.0074'
-                ]
-            ],
-            'Test 8 location found' => [
-                gethostbyname('bing.com'),
-                [
+                ]],
+            'Test 8 location found'     => [gethostbyname('bing.com'), [
                     'city' => 'Redmond, Washington',
                     'lat' => '47.6801',
                     'lng' => '-122.1206'
-                ]
-            ]
+                ]]
         ];
     }
 
