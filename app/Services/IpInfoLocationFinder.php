a383e5fb4125c035397d3138d04f51196f71bb8d
Victor Lantigua <victor@Victors-MacBook-Pro.local>
2016-12-15 10:46:01 -0500

    Rename IpInfoService to IpInfoLocationFinder

---
+++ app/Services/IpInfoLocationFinder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\LocationFinderInterface;
+
+/**
+ * This service class is a client for ipinfo.io IP lookup API.
+ *
+ * @author Victor Lantigua
+ * @link https://ipinfo.io/developers For developer documentation
+ */
+class IpInfoLocationFinder implements LocationFinderInterface
+{
+    /**
+     * The API URL.
+     */
+    const API_URL = 'http://ipinfo.io';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function ipToGeolocation($ip)
+    {
+        $geolocation = [
+            'city' => null,
+            'lat' => null,
+            'lng' => null
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, self::API_URL . '/' . $ip . '/json');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $body = curl_exec($ch);
+        $headers = curl_getinfo($ch);
+        curl_close($ch);
+        $decodedBody = json_decode($body, true);
+
+        if (!(empty($decodedBody['loc']) && empty($decodedBody['city']) && empty($decodedBody['region']) && empty($decodedBody['country']))) {
+            list($geolocation['lat'], $geolocation['lng']) = explode(',', $decodedBody['loc']);
+            $geolocation['city'] = $decodedBody['city'] . ', ';
+            $geolocation['city'] .= ('US' === $decodedBody['country']) ? $decodedBody['region'] : $decodedBody['country'];
+        }
+
+        return $geolocation;
+    }
+}
